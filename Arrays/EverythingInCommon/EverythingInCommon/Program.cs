0dd2c594d5d227c826d368648e56716ddeeac441
Kardam Simeonov <masutayodagm@gmail.com>
2021-09-23 12:33:16 +0300

    Updated Repo

---
+++ Arrays/EverythingInCommon/EverythingInCommon/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace EverythingInCommon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (firstArray.Length < secondArray.Length)
+            {
+                Console.WriteLine("Arrays are not identical.");
+                Console.WriteLine($"Found difference at {firstArray.Length - 1} index");
+            }
+            else if (firstArray.Length > secondArray.Length)
+            {
+                Console.WriteLine("Arrays are not identical.");
+                Console.WriteLine($"Found difference at {secondArray.Length - 1} index");
+            }
+            else
+            {
+                for (int i = 0; i < firstArray.Length; i++)
+                {
+                    if (firstArray[i] != secondArray[i])
+                    {
+                        Console.WriteLine("Arrays are not identical.");
+                        Console.WriteLine($"Found difference at {i} index");
+                        System.Environment.Exit(0);
+                    }
+                }
+            }
+
+            Console.WriteLine("Arrays are identical.");
+            Console.WriteLine($"Sum: {firstArray.Sum()}");
+        }
+    }
+}
---
+++ Arrays/NothingInCommon/NothingInCommon/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NothingInCommon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Tuple<int, int>> valuePairs = new HashSet<Tuple<int, int>>();
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+                for (int n = i + 1; n < arr.Length; n++)
+                    if (arr[i] + arr[n] == sum)
+                        valuePairs.Add(new Tuple<int, int> (arr[i], arr[n] ));
+
+            foreach (var pair in valuePairs)
+                Console.WriteLine($"{pair.Item1} {pair.Item2}");
+        }
+    }
+}
