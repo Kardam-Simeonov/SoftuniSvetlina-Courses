d31edc32d6b8b10a83206d3b28cb653413c38f81
Kardam Simeonov <masutayodagm@gmail.com>
2021-11-25 13:25:09 +0200

    update

---
+++ C#/OOP/06.Circular-Queue-Skeleton/CircularQueue/CircularQueue.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+public class CircularQueue
+{
+    private int[] elements;
+    private int startIndex;
+    private int endIndex;
+
+    public int Count { get; private set; }
+
+    private const int InitialCapacity = 16;
+    public CircularQueue(int capacity = InitialCapacity)
+    {
+        elements = new int[capacity];
+    }
+
+    public void Enqueue(int element)
+    {
+        if (Count >= elements.Length)
+            Grow();
+
+        elements[endIndex] = element;
+        endIndex = (endIndex + 1) % elements.Length;
+
+        Count++;
+    }
+
+    private void Grow()
+    {
+        int[] newElements = new int[2 * elements.Length];
+        CopyAllElementsTo(newElements);
+
+        elements = newElements;
+        startIndex = 0;
+        endIndex = Count;
+    }
+
+    private void CopyAllElementsTo(int[] resultArr)
+    {
+        int sourceIndex = startIndex;
+        int destinationIndex = 0;
+
+        for (int i = 0; i < Count; i++)
+        {
+            resultArr[destinationIndex] = elements[sourceIndex];
+            sourceIndex = (sourceIndex + 1) % elements.Length;
+            destinationIndex++;
+        }
+    }
+
+    public int Dequeue()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("The queue is empty!");
+
+        int result = elements[startIndex];
+        startIndex = (startIndex + 1) % elements.Length;
+        Count--;
+
+        return result;
+    }
+
+    public int[] ToArray()
+    {
+        int[] result = new int[Count];
+        CopyAllElementsTo(result);
+        return result;
+    } 
+}
+
+
+class Example
+{
+    static void Main()
+    {
+        var queue = new CircularQueue();
+
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        queue.Enqueue(5);
+        queue.Enqueue(6);
+
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        var first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-7);
+        queue.Enqueue(-8);
+        queue.Enqueue(-9);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-10);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+    }
+}
---
+++ C#/OOP/06.Implementing-Stack-Exercises/Lab_ImplementingStack/ImplementingCustomStack/CustomStack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementCustomStack
+{
+    public class CustomStack
+    {
+        private const int INITIAL_CAPACITY = 4;
+        private int[] items;
+        private int count;
+
+        public int Count { get => this.count; }
+        public CustomStack()
+        {
+            this.count = 0;
+            this.items = new int[INITIAL_CAPACITY];
+        }
+       
+        public void Push(int element)
+        {
+            if (this.items.Length == this.count)
+                Resize();
+
+            this.items[this.count] = element;
+            count++;
+        }
+
+        public int Pop()
+        {
+            if (this.items.Length == 0)
+                throw new InvalidOperationException("CustomStack is empty");
+
+            int lastElement = this.items[count - 1];
+            Array.Resize(ref this.items, this.items.Length - 1);
+            count--;
+
+            return lastElement;
+        }
+
+        public int Peek()
+        {
+            if (this.items.Length == 0)
+                throw new InvalidOperationException("CustomStack is empty");
+
+            return this.items[count - 1];
+        }
+
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+        public void Resize()
+        {
+            int[] newArr = new int[this.items.Length];
+            Array.Copy(this.items, newArr, newArr.Length);
+            this.items = newArr;
+        }
+    }
+}
