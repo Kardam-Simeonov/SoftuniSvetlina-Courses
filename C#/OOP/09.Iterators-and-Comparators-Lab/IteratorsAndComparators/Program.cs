0bf48c89afdbc50515b6b8fe92aba4d3d6b5b9d7
Kardam Simeonov <masutayodagm@gmail.com>
2021-12-14 09:19:42 +0200

    update

--- C#/OOP/09.Iterators-and-Comparators-Lab/IteratorsAndComparators/Program.cs
+++ C#/OOP/09.Iterators-and-Comparators-Lab/IteratorsAndComparators/Program.cs
@@ -10,8 +10,10 @@ namespace IteratorsAndComparators
             Book bookTwo = new Book("The Documents in the Case", 2002,
             "Dorothy Sayers", "Robert Eustace");
             Book bookThree = new Book("The Documents in the Case", 1930);
+
             Library libraryOne = new Library();
             Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
             foreach (var book in libraryTwo)
             {
                 Console.WriteLine(book.Title);
