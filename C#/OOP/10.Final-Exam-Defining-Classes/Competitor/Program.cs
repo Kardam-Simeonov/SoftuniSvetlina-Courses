02409af9135711fa14739564d59caf9c03637117
Kardam Simeonov <masutayodagm@gmail.com>
2021-12-21 10:10:21 +0200

    update

---
+++ C#/OOP/10.Final-Exam-Defining-Classes/Competitor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ImplementCompetitor
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Competitor<int> IvanTheWiner = new Competitor<int>("Ivan", 16);
+            Competitor<int> Vanya = new Competitor<int>("Vanya", 17);
+            IvanTheWiner.Add(6);
+            IvanTheWiner.Add(2);
+            IvanTheWiner.Add(5);
+            IvanTheWiner.Add(4);
+            Console.WriteLine(IvanTheWiner.CompareTo(Vanya));
+        }
+    }
+}
