f5e650887131d7c1812ecd3a5dff70ef8853b970
KardamSimeonov <masutayodagm@gmail.com>
2022-01-18 10:49:44 +0200

    update

---
+++ HTML_CSS/BootstrapPortfolio/app.js
@@ -0,0 +1,20 @@
+function autoCycle(){
+    $('.carousel').carousel('cycle');
+}
+
+function next(){
+    $('.carousel').carousel('next');
+}
+
+function prev(){
+    $('.carousel').carousel('prev');
+}
+
+function showToast(){
+    $('.toast').toast('show');
+   
+}
+
+function init(){
+    $('[data-toggle="popover"]').popover();
+}
