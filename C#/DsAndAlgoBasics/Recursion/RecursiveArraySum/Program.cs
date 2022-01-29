9062d17eb5edb59a55587f9e785f90f3c4f81c17
KardamSimeonov <masutayodagm@gmail.com>
2022-01-29 22:45:19 +0200

    update

---
+++ C#/DsAndAlgoBasics/Recursion/RecursiveArraySum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(FindSum(arr, 0));
+        }
+
+        static int FindSum(int[] arr, int index)
+        {
+            if (index == arr.Length - 1)
+                return arr[index];
+
+            return arr[index] + FindSum(arr, index + 1);
+        }
+    }
+}
---
+++ C#/DsAndAlgoBasics/Recursion/ReverseArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(ReverseArray(arr, arr.Length - 1));
+        }
+
+        static string ReverseArray(int[] arr, int index)
+        {
+            if (index == 0)
+                return $"{arr[index]}";
+
+            return $"{arr[index]} {ReverseArray(arr, index - 1)}";
+        }
+    }
+}
