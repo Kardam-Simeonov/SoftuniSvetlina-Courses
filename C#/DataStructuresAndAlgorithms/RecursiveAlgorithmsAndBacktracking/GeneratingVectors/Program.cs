33836f9e45e33b1fe45dcfc013c6808615b1e976
KardamSimeonov <masutayodagm@gmail.com>
2022-02-10 16:36:17 +0200

    update

---
+++ C#/DataStructuresAndAlgorithms/RecursiveAlgorithmsAndBacktracking/GeneratingVectors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GeneratingVectors
+{
+    internal class Program
+    {
+        static int numberOfLoops;
+        static int[] loops;
+
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            numberOfLoops = input;
+
+            loops = new int[numberOfLoops];
+
+            VectorLoop(0);
+        }
+
+        static void VectorLoop(int currentLoop)
+        {
+            if (currentLoop == numberOfLoops)
+            {
+                for (int i = 0; i < numberOfLoops; i++)
+                    Console.Write($"{loops[i]}");
+
+                Console.WriteLine();
+                return;
+            }
+
+            for (int counter = 0; counter <= 1; counter++)
+            {
+                loops[currentLoop] = counter;
+                VectorLoop(currentLoop + 1);
+            }
+        }
+    }
+}
