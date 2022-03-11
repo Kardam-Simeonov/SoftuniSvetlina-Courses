a000ba520b2267a36bc6a611cbaefaba2d9681cb
Kardam Simeonov <masutayodagm@gmail.com>
2022-03-11 09:43:54 +0200

    update

---
+++ C#/DataStructuresAndAlgorithms/05.Intro_To_Data_Structures/IntroToDataStructures-Exercise/WordsWithPrefix/Program.cs
@@ -0,0 +1,27 @@
+﻿using rm.Trie;
+using System;
+
+namespace WordsWithPrefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trie trie = new Trie();
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string prefix = Console.ReadLine();
+
+            foreach (string word in input)
+            {
+                trie.AddWord(word);
+            }
+
+            Console.WriteLine(trie.Count());
+            Console.WriteLine(trie.UniqueCount());
+            Console.WriteLine(string.Join(", ", trie.GetWords()));
+            Console.WriteLine(string.Join(", ", trie.GetWords(prefix)));
+            trie.RemovePrefix(prefix);
+            Console.WriteLine(string.Join(", ", trie.GetWords()));
+        }
+    }
+}
