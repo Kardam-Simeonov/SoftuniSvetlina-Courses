951a29705a7ed7646b24df8a66546c3b0c8d3b11
KardamSimeonov <masutayodagm@gmail.com>
2022-03-12 23:00:44 +0200

    update

---
+++ C#/DataStructuresAndAlgorithms/05.Intro-To-Data-Structures/Intro-To-Data-Structures-Exercise/EventsInGivenDateRange/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using Wintellect.PowerCollections;
+
+namespace EventsInGivenDateRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            var events = new OrderedMultiDictionary<DateTime, string>(true);
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] eventTokens = Console.ReadLine().Split(" | ");
+                string eventName = eventTokens[0];
+                DateTime eventDate = DateTime.Parse(eventTokens[1]);
+                events.Add(eventDate, eventName);
+            }
+
+            string[] datesTokens = Console.ReadLine().Split(" | ");
+            DateTime startDate = DateTime.Parse(datesTokens[0]);
+            DateTime endDate = DateTime.Parse(datesTokens[1]);
+            var eventsInRange = events.Range(startDate, true, endDate, true);
+
+            foreach (var dateTime in eventsInRange)
+            {
+                Console.WriteLine($"{dateTime.Value} | {dateTime.Key.ToString("dd-MMM-yyyy hh:mm")}");
+            }
+        }
+    }
+}
