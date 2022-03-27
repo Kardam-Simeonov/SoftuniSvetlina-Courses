c0dfb56171cf724c0e63fd2bca680168b94bdff6
KardamSimeonov <masutayodagm@gmail.com>
2022-03-27 20:12:21 +0300

    Added Graph-Lab

--- C#/DataStructuresAndAlgorithms/07.Graphs/Graphs-Lab/TraverseGraphWithBFS/Program.cs
+++ C#/DataStructuresAndAlgorithms/07.Graphs/Graphs-Lab/TraverseGraphWithBFS/Program.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace TraverseGraphWithDFS
+namespace TraverseGraphWithBFS
 {
     class Program
     {
         static void Main(string[] args)
         {
             Graph graph = ReadGraph();
-
+            
             int startVertex = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Following is Depth First Traversal(starting from vertex {startVertex})");
+            Console.WriteLine($"Following is Breadth First Traversal(starting from vertex {startVertex})");
 
-            graph.DFS(startVertex);
+            graph.BFS(startVertex);
         }
 
         public static Graph ReadGraph()
@@ -34,42 +34,48 @@ namespace TraverseGraphWithDFS
         }
     }
     
+    
     class Graph
     {
         private static int _verticesCount;
-        private static List<int>[] _adjacents;
+        private static LinkedList<int>[] _adjacents;
         
         public Graph(int verticesCount)
         {
-            _adjacents = new List<int>[verticesCount];
+            _adjacents = new LinkedList<int>[verticesCount];
             for (int i = 0; i < _adjacents.Length; i++)
-                _adjacents[i] = new List<int>();
+                _adjacents[i] = new LinkedList<int>();
 
             _verticesCount = verticesCount;
         }
 
         public void AddEdge(int firstVertex, int secondVertex)
         {
-            _adjacents[firstVertex].Add(secondVertex);
+            _adjacents[firstVertex].AddLast(secondVertex);
         }
 
-        public void DFS(int vertex)
+        public void BFS(int vertex)
         {
-            bool[] visited = new bool[_verticesCount];
+            bool[] visitedVertices = new bool[_verticesCount];
+            var queue = new Queue<int>();
 
-            DFSUtil(vertex, visited);
-        }
+            visitedVertices[vertex] = true;
+            queue.Enqueue(vertex);
 
-        void DFSUtil(int vertex, bool[] visited)
-        {
-            visited[vertex] = true;
-            Console.Write(vertex + " ");
-
-            List<int> verticesList = _adjacents[vertex];
-            foreach (var v in verticesList)
+            while (queue.Any())
             {
-                if (!visited[v])
-                    DFSUtil(v, visited);
+                vertex = queue.Dequeue();
+                Console.Write(vertex + " ");
+                LinkedList<int> list = _adjacents[vertex];
+
+                foreach (var adjacent in list)
+                {
+                    if (!visitedVertices[adjacent])
+                    {
+                        visitedVertices[adjacent] = true;
+                        queue.Enqueue(adjacent);
+                    }
+                }
             }
         }
     }
