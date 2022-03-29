ade2e806902381c3c409be2938b2b3dc10936fa3
Kardam Simeonov <masutayodagm@gmail.com>
2022-03-29 11:16:56 +0300

    Added ASP.NET Lab

---
+++ SoftTech-Backend/ASP.NET-MVC/ASP.NET-MVC-Introduction-Lab/ASP.NET-MVC-Introduction-Lab/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using ASP.NET_MVC_Introduction_Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NET_MVC_Introduction_Lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SoftTech-Backend/ASP.NET-MVC/ASP.NET-MVC-Introduction-Lab/ASP.NET-MVC-Introduction-Lab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_MVC_Introduction_Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
