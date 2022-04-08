1d58ce018c47890ae2efabad9ac8f1fb68530212
KardamSimeonov <masutayodagm@gmail.com>
2022-04-08 18:31:48 +0300

    Added ASP.NET-MVC Exercise

--- SoftTech-Backend/ASP.NET-MVC/07.ASP.NET-MVC-Introduction-Exercises-Resources/CalculatorApp-Resources/CalculatorApp/Controllers/HomeController.cs
+++ SoftTech-Backend/ASP.NET-MVC/07.ASP.NET-MVC-Introduction-Exercises-Resources/CalculatorApp-Resources/CalculatorApp/Controllers/HomeController.cs
@@ -1,13 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace CalculatorApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(Calculator calculator)
         {
-            return View();
+            return View(calculator);
+        }
+        
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
         }
 
+        private decimal CalculateResult(Calculator calculator)
+        {
+            decimal result = 0;
+
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
     }
 }
---
+++ SoftTech-Backend/ASP.NET-MVC/07.ASP.NET-MVC-Introduction-Exercises-Resources/CalculatorApp-Resources/CalculatorApp/Models/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorApp.Models
+{
+    public class Calculator
+    {
+        public decimal LeftOperand { get; set; }
+        public decimal RightOperand { get; set; }
+        public string Operator { get; set; }
+        public decimal Result { get; set; }
+    }
+}
