9261bd6f39925f9785f148792ccd0b5686c5eef4
Kardam Simeonov <masutayodagm@gmail.com>
2022-04-28 13:25:06 +0300

    Updated Tree and Graph Lab

---
+++ C#/DataStructuresAndAlgorithms/06.Trees-BFS-DFS/Trees-BFS-DFS-Lab/CheckNumberOccurences/Program.cs
@@ -0,0 +1,52 @@
+﻿using SimpleTreeNode;
+using System;
+
+namespace CheckNumberOccurences
+{
+    internal class Program
+    {
+        static int target;
+        static int count;
+        static void Main(string[] args)
+        {
+            target = int.Parse(Console.ReadLine());
+            count = 0;
+            TreeNode<int> tree = new TreeNode<int>(1,
+                new TreeNode<int>(1,
+                    new TreeNode<int>(8),
+                    new TreeNode<int>(1)
+                    ),
+                new TreeNode<int>(7,
+                    new TreeNode<int>(8)
+                    ),
+                new TreeNode<int>(7,
+                    new TreeNode<int>(5),
+                    new TreeNode<int>(3)
+                    )
+                );
+
+            CountOccurence(tree);
+        }
+
+        private static void CountOccurence(TreeNode<int> tree)
+        {
+            DFS(tree);
+
+            if (count == 0)
+                Console.WriteLine($"Element {target} doesn’t exist in the tree.");
+            else
+                Console.WriteLine($"Element {target} occurs {count} times.");
+        }
+
+        static void DFS(TreeNode<int> node)
+        {
+            if (node.Value == target)
+                count++;
+
+            foreach (var child in node.ChildNodes)
+            {
+                DFS(child);
+            }
+        }
+    }
+}
---
+++ C#/DataStructuresAndAlgorithms/07.Graphs/Graphs-Exercise/ConnectedGraph/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConnectedGraph
+{
+    internal class Program
+    {
+        private static bool[] visited;
+        private static List<int>[] graph;
+
+        static void Main(string[] args)
+        {
+            graph = ReadGraph();
+            CheckIfConnected();
+        }
+
+        private static List<int>[] ReadGraph()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var graph = new List<int>[n + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                graph[i] = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+            }
+
+            return graph;
+        }
+
+        private static void CheckIfConnected()
+        {
+            visited = new bool[graph.Length];
+            DFS(0);
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                if (!visited[i])
+                {
+                    Console.WriteLine("The graph is not connected.");
+                    return;
+                }
+            }
+            Console.WriteLine("The graph is connected.");
+        }
+
+        private static void DFS(int vertex)
+        {
+            if (!visited[vertex])
+            {
+                visited[vertex] = true;
+                foreach (var child in graph[vertex])
+                {
+                    DFS(child);
+                }
+            }
+        }
+
+    }
+}
