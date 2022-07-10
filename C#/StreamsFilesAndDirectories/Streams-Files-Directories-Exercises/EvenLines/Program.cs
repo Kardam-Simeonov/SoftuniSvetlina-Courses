2636a12e4002a9b4ed9d8d8045c470adf9c0a334
KardamSimeonov <masutayodagm@gmail.com>
2022-07-10 22:12:32 +0300

    update

---
+++ C#/StreamsFilesAndDirectories/Streams-Files-Directories-Exercises/EvenLines/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace EvenLines
+{
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+        public static string ProcessLines(string inputFilePath)
+        {
+            var lines = new List<string>();
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                string line = reader.ReadLine();
+                int counter = 0;
+
+                while (line != null)
+                {
+                    if (counter % 2 == 0)
+                        lines.Add(line);
+
+                    counter++;
+                    line = reader.ReadLine();
+                }
+
+                foreach (string item in lines)
+                    Regex.Replace(item, "[-,.!?]", "@");
+
+                lines.Reverse();
+            }
+
+            return String.Join("\n", lines);
+        }
+    }
+}
---
+++ C#/StreamsFilesAndDirectories/Streams-Files-Directories-Exercises/LineNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int counter = 1;
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine($"Line {counter}: {line}({Regex.Matches(line, "[a-z]").Count})({Regex.Matches(line, "[-,.!?]").Count})");
+                counter++;
+            }
+        }
+    }
+}
