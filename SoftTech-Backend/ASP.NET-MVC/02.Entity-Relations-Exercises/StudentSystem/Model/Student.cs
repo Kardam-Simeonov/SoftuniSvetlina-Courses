62162fa4bf4e8bfcb55d8911853efc56aeabd21d
Kardam Simeonov <masutayodagm@gmail.com>
2022-10-06 12:51:20 +0300

    update

---
+++ SoftTech-Backend/ASP.NET-MVC/02.Entity-Relations-Exercises/StudentSystem/Model/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem.Data.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        []
+        public int PhoneNumber { get; set; }
+
+    }
+}
---
+++ SoftTech-Backend/ASP.NET-MVC/02.Entity-Relations-Exercises/StudentSystem/StudentSystemContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data
+{
+    internal class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
