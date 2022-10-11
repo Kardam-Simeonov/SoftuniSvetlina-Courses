53532e58fdd243534eb3981e0cc1bd73228d525b
KardamSimeonov <masutayodagm@gmail.com>
2022-10-11 22:49:03 +0300

    update

---
+++ SoftTech-Backend/ORM-and-Entity-Framework/02.Entity-Relations/02.Entity-Relations-Exercise/StudentSystem/Data/Models/Course.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem.Data.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
---
+++ SoftTech-Backend/ORM-and-Entity-Framework/02.Entity-Relations/02.Entity-Relations-Exercise/StudentSystem/Data/Models/Homework.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem.Data.Models
+{
+    public class Homework
+    {
+        [Key]
+        public int HomeworkId { get; set; }
+
+        [Required]
+        [Unicode(false)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime SubmissionTime { get; set; }
+
+        [Required]
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+
+        [Required]
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+    }
+}
