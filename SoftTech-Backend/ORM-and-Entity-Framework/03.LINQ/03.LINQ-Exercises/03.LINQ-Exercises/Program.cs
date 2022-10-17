c45da828f916d6b09f90aabd033ce8009609119d
KardamSimeonov <masutayodagm@gmail.com>
2022-10-17 21:16:28 +0300

    update

--- SoftTech-Backend/ORM-and-Entity-Framework/03.LINQ/03.LINQ-Exercises/03.LINQ-Exercises/Program.cs
+++ SoftTech-Backend/ORM-and-Entity-Framework/03.LINQ/03.LINQ-Exercises/03.LINQ-Exercises/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MusicHub.Data;
 using MusicHub.Data.Models;
+using System.Text;
 
 namespace MusicHub
 {
@@ -12,9 +13,48 @@ namespace MusicHub
 
         public string ExportAlbumsInfo(MusicHubContext context, int producerId)
         {
-            var albumsByProducer = context.Albums
+            var albumsByProducer = context.Albums.Where(a => a.ProducerId == producerId).ToList();
 
-            return string.Empty;
+            var stringBuilder = new StringBuilder();
+
+            foreach (var album in albumsByProducer)
+            {
+                stringBuilder.AppendLine($"-AlbumName: {album.Name}");
+                stringBuilder.AppendLine($"-ReleaseDate: {album.ReleaseDate.ToString("MM/dd/yyyy")}");
+                stringBuilder.AppendLine($"-ProducerName: {album.Producer?.Name ?? "Unknown"}");
+                stringBuilder.AppendLine($"-Songs:");
+
+                foreach (var song in album.Songs?.OrderBy(s => s.Id))
+                {
+                    stringBuilder.AppendLine($"---#{song.Id}");
+                    stringBuilder.AppendLine($"---SongName: {song.Name}");
+                    stringBuilder.AppendLine($"---Price: {song.Price:F2}");
+                    stringBuilder.AppendLine($"---Writer: {song.Writer?.Name ?? "Unknown"}");
+                }
+
+                stringBuilder.AppendLine($"-AlbumPrice: {album.Price:F2}");
+            }
+
+            return stringBuilder.ToString();
+        }
+        
+        public string ExportSongsAboveDuration(MusicHubContext context, int duration)
+        {
+            var songsAboveDuration = context.Songs.Where(s => s.Duration > new TimeSpan(duration)).ToList().OrderBy(s => s.Id);
+
+            var stringBuilder = new StringBuilder();
+
+            foreach (var song in songsAboveDuration)
+            {
+                stringBuilder.AppendLine($"-Song #{song.Id}");
+                stringBuilder.AppendLine($"---SongName: {song.Name}");
+                stringBuilder.AppendLine($"---Writer: {song.Writer.Name}");
+                stringBuilder.AppendLine($"---Performer: {song.SongPerformers.Select(sp => sp.Performer.FirstName + " " + sp.Performer.LastName).FirstOrDefault()}");
+                stringBuilder.AppendLine($"---AlbumProducer: {song.Album.Producer.Name}");
+                stringBuilder.AppendLine($"---Duration: {song.Duration}");
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
