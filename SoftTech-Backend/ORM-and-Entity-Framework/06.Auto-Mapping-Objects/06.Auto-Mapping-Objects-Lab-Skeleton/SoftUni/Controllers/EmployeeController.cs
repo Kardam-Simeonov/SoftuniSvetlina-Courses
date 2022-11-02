12fbca698ce95e7a7e530bb0fa264c6b25b21fc9
Kardam Simeonov <masutayodagm@gmail.com>
2022-11-02 13:19:43 +0200

    update

---
+++ SoftTech-Backend/ORM-and-Entity-Framework/06.Auto-Mapping-Objects/06.Auto-Mapping-Objects-Lab-Skeleton/SoftUni/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using SoftUni.Data;
+using SoftUni.Dto;
+using SoftUni.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni.Controllers
+{
+    public class EmployeeController
+    {
+        private readonly SoftUniContext context;
+
+        public EmployeeController(SoftUniContext context)
+        {
+        }
+
+        public EmployeeDtoViewModel GetEmployeeInfo(int id)
+        {
+            //Initialize the mapper
+            var config = new MapperConfiguration(cfg =>
+                cfg.CreateMap<Employee, EmployeeDtoViewModel>());
+            var mapper = config.CreateMapper();
+
+            //Using automapper
+            var employee = context.Employees.Where(e => e.EmployeeId == id).FirstOrDefault();
+            var dto = mapper.Map<EmployeeDtoViewModel>(employee);
+
+            return dto;
+        }
+    }
+}
---
+++ SoftTech-Backend/ORM-and-Entity-Framework/06.Auto-Mapping-Objects/06.Auto-Mapping-Objects-Lab-Skeleton/SoftUni/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using SoftUni.Controllers;
+using SoftUni.Data;
+using SoftUni.Dto;
+using SoftUni.Models;
+using System;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var context = new SoftUniContext();
+
+            Console.WriteLine("Write the id of an Employee to get his information.");
+
+            var employeeInfoRequest = int.Parse(Console.ReadLine());
+
+            var employeeController = new EmployeeController(context);
+
+            var employeeInfoResult = employeeController.GetEmployeeInfo(employeeInfoRequest);
+        }
+    }
+}
