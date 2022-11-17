22f0172a41c410658ff9d3bc4b3a9b8562bf555e
Kardam Simeonov <masutayodagm@gmail.com>
2022-11-17 09:23:04 +0200

    update

--- SoftTech-Frontend/Javascript/Text-Processing/Text-Processing-Exercise.js
+++ SoftTech-Frontend/Javascript/Text-Processing/Text-Processing-Exercise.js
@@ -73,3 +73,11 @@ function pascalCaseSplitter(text){
 
     console.log(splitString.join(', '));
 }
+
+function cutAndReverse(input){
+    let firstHalf = input.substring(0, input.length / 2).split('').reverse().join('');
+    let secondHalf = input.substring(input.length / 2).split('').reverse().join('');
+
+    console.log(firstHalf);
+    console.log(secondHalf);
+}
