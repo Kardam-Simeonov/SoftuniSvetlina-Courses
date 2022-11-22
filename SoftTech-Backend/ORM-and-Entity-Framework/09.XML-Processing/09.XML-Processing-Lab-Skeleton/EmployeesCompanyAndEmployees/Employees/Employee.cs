b9526cecf7a73cbca4fb4009b1eeffbdaf7ec57f
KardamSimeonov <masutayodagm@gmail.com>
2022-11-22 20:12:11 +0200

    update

--- SoftTech-Backend/ORM-and-Entity-Framework/09.XML-Processing/09.XML-Processing-Lab-Skeleton/EmployeesCompanyAndEmployees/Employees/Employee.cs
+++ SoftTech-Backend/ORM-and-Entity-Framework/09.XML-Processing/09.XML-Processing-Lab-Skeleton/EmployeesCompanyAndEmployees/Employees/Employee.cs
@@ -6,8 +6,10 @@ namespace Employees
     {
         public int Id { get; set; }
 
+        [XmlElement("Name")]
         public string EmployeeName { get; set; }
 
+        [XmlElement("Department")]
         public string DepartmentName { get; set; }
     }
 }
--- SoftTech-Backend/ORM-and-Entity-Framework/09.XML-Processing/09.XML-Processing-Lab-Skeleton/EmployeesCompanyAndEmployees/Employees/StartUp.cs
+++ SoftTech-Backend/ORM-and-Entity-Framework/09.XML-Processing/09.XML-Processing-Lab-Skeleton/EmployeesCompanyAndEmployees/Employees/StartUp.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
 using System.Xml;
+using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace Employees
@@ -10,7 +14,52 @@ namespace Employees
     {
         static void Main()
         {
+            Console.WriteLine(ReadEmployeesData("../../../XMLData.xml"));
+            WriteEmployeesData("../../../ArrayOfEmployees.xml");
+        }
+        public static string ReadEmployeesData(string path)
+        {
+            var employees = XDocument.Load(path).Root.Elements();
+            StringBuilder result = new StringBuilder();
+            foreach (var employee in employees)
+            {               
+                result.AppendLine($"{employee.Element("Id").Value} {employee.Element("Name").Value} {employee.Element("Department").Value}");
+            }
+            return result.ToString().TrimEnd();
+        }
+        
+        public static void WriteEmployeesData(string path)
+        {
+            Employee employee1 = new Employee()
+            {
+                Id = 1,
+                EmployeeName = "Alisa Bell",
+                DepartmentName = "Development"
+            };
+            Employee employee2 = new Employee()
+            {
+                Id = 2,
+                EmployeeName = "Samuel Griffin",
+                DepartmentName = "Development"
+            };
+            Employee employee3 = new Employee()
+            {
+                Id = 3,
+                EmployeeName = "Johnny Garcia",
+                DepartmentName = "Development"
+            };
 
+
+            XDocument output = new XDocument();
+            using (var writer = output.CreateWriter())
+            {
+                var serializer = new DataContractSerializer(employee1.GetType());
+                serializer.WriteObject(writer, employee1);
+                serializer.WriteObject(writer, employee2);
+                serializer.WriteObject(writer, employee3);
+            }
+
+            output.Save(path);
         }
     }
 }
