038af7b0716f9edc99c92f38748501608862d323
Kardam Simeonov <masutayodagm@gmail.com>
2022-11-22 13:47:17 +0200

    update

--- Android-Studio/SudokuInterface/app/src/main/java/com/example/sudokuinterface/MainActivity.java
+++ Android-Studio/SudokuInterface/app/src/main/java/com/example/sudokuinterface/MainActivity.java
@@ -7,14 +7,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.HashSet;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-
-
+    TextView[][] sudokuMatrix = new TextView[9][9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int row = 0; row < 9; row++){
+            for (int col = 0; col < 9; col++){
+                int id = getResources().getIdentifier(String.format("txt_%d_%d", row, col), "id", getPackageName());
+                TextView current = findViewById(id);
+                sudokuMatrix[row][col] = current;
+            }
+        }
 
     }
 }
