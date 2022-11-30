267ed873f76b1eded5e6ac0c505b9327b16ad0d3
Kardam Simeonov <masutayodagm@gmail.com>
2022-11-30 11:00:12 +0200

    update

--- Android-Studio/SudokuInterface/app/src/main/java/com/example/sudokuinterface/MainActivity.java
+++ Android-Studio/SudokuInterface/app/src/main/java/com/example/sudokuinterface/MainActivity.java
@@ -14,6 +14,10 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     TextView[][] sudokuMatrix = new TextView[9][9];
+    TextView[] keypadButtons = new TextView[11];
+    boolean hasCurrentlySelected = false;
+    TextView currentlySelected = null;
+    String currentTextStorage = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +28,53 @@ public class MainActivity extends AppCompatActivity {
             for (int col = 0; col < 9; col++){
                 int id = getResources().getIdentifier(String.format("txt_%d_%d", row, col), "id", getPackageName());
                 TextView current = findViewById(id);
+                current.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!hasCurrentlySelected){
+                            currentlySelected = current;
+                            currentTextStorage = current.getText().toString();
+                            hasCurrentlySelected = true;
+                        }
+                    }
+                });
+
                 sudokuMatrix[row][col] = current;
             }
         }
 
+        for (int i = 1; i <= 9; i++){
+            int id = getResources().getIdentifier(String.format("keyNum%d", i), "id", getPackageName());
+            TextView current = findViewById(id);
+
+            current.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (hasCurrentlySelected){
+                        currentlySelected.setText(current.getText());
+                    }
+                }
+            });
+            keypadButtons[i - 1] = current;
+        }
+        keypadButtons[9] = findViewById(getResources().getIdentifier(String.format("keyEdit"), "id", getPackageName()));
+        keypadButtons[9].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (hasCurrentlySelected){
+                    hasCurrentlySelected = false;
+                }
+            }
+        });
+        keypadButtons[10] = findViewById(getResources().getIdentifier(String.format("keyDelete"), "id", getPackageName()));
+        keypadButtons[10].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (hasCurrentlySelected) {
+                    currentlySelected.setText(currentTextStorage);
+                    hasCurrentlySelected = false;
+                }
+            }
+        });
     }
 }
