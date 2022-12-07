09a95ce2e8a3aa7b5e3fed67c3a4ef143e119716
Kardam Simeonov <masutayodagm@gmail.com>
2022-12-07 13:59:54 +0200

    update

---
+++ SoftTech-Backend/ASP.NET-MVC/01.ASP.NET-Core-Introduction/ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,37 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> Messages = new();
+        
+        public IActionResult Show()
+        {
+            if (Messages.Count() < 1)
+                return View(new ChatViewModel());
+            
+            ChatViewModel chatModel = new()
+            {
+                // Ensuring that Messages is a collection of MessageViewModel
+                Messages = Messages.Select(m => new MessageViewModel
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                }).ToList()
+            }; 
+            return View(chatModel);
+        }
+        
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            MessageViewModel message = chat.CurrentMessage;
+
+            Messages.Add(new KeyValuePair<string, string>(message.Sender, message.MessageText));
+
+            return RedirectToAction("Show");
+        }
+    }
+}
