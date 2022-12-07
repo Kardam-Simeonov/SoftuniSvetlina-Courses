0cfb26c3ff5c53ece31e28fa9490ab1ee3ec6690
Kardam Simeonov <masutayodagm@gmail.com>
2022-12-07 14:01:54 +0200

    update

--- SoftTech-Backend/ASP.NET-MVC/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
+++ SoftTech-Backend/ASP.NET-MVC/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace HouseRentingSystem.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
