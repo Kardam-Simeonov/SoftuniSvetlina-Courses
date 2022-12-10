e5c17d37255ac9749d5d5e9c9de79e5c14b87f38
KardamSimeonov <masutayodagm@gmail.com>
2022-12-10 20:03:18 +0200

    update

--- SoftTech-Backend/SQL/08.Triggers-And-Transactions/08.Triggers-And-Transactions-Exercises.sql
+++ SoftTech-Backend/SQL/08.Triggers-And-Transactions/08.Triggers-And-Transactions-Exercises.sql
@@ -1,3 +1,6 @@
+----------------------------
+-- Queries for Bank Database
+USE Bank;
 -- 1. Create Table Logs
 CREATE TABLE Logs(
 	Id int PRIMARY KEY,
@@ -46,19 +49,18 @@ BEGIN
 	);
 END;
 
-CREATE PROC usp_DepositMoney (@AccountId INT, @MoneyAmount DECIMAL(16,2))
+-- 3. Deposit Money
+CREATE PROC usp_DepositMoney (@AccountId INT, @MoneyAmount DECIMAL(16,4))
 AS
 BEGIN TRANSACTION
-IF (@recieverAccountId IS NULL OR @senderAccountId IS NULL)
+IF (@MoneyAmount < 0)
 BEGIN
 	ROLLBACK
-	RAISERROR('Invalid account', 16, 1);
+	RAISERROR('Invalid Amount!', 16, 1);
 	RETURN
 END
-UPDATE Accounts SET Balance = Balance - @amount
-	WHERE AccountId = @senderAccountId;
-UPDATE Accounts SET Balance = Balance + @amount
-	WHERE AccountId = @recieverAccountId;
+UPDATE Accounts SET Balance = Balance + @MoneyAmount
+	WHERE Id = @AccountId;
 IF @@ROWCOUNT <> 1 -- Didn’t affect exactly one row
 BEGIN
 	ROLLBACK
@@ -66,3 +68,95 @@ BEGIN
 	RETURN
 END
 COMMIT
+
+-- 4. Withdraw Money
+CREATE PROC usp_WithdrawMoney (@AccountId INT, @MoneyAmount DECIMAL(16,4))
+AS
+BEGIN TRANSACTION
+IF (@MoneyAmount < 0)
+BEGIN
+	ROLLBACK
+	RAISERROR('Invalid Amount!', 16, 1);
+	RETURN
+END
+UPDATE Accounts SET Balance = Balance - @MoneyAmount
+	WHERE Id = @AccountId;
+IF @@ROWCOUNT <> 1 -- Didn’t affect exactly one row
+BEGIN
+	ROLLBACK
+	RAISERROR('Invalid operation!', 16, 2);
+	RETURN
+END
+COMMIT
+
+-- 5. Money Transfer
+CREATE PROC usp_TransferMoney(@SenderId INT, @ReceiverId INT, @Amount DECIMAL(16,4))
+AS
+BEGIN TRANSACTION
+IF (@Amount < 0)
+BEGIN
+	ROLLBACK
+	RAISERROR('Invalid Amount!', 16, 1);
+	RETURN
+END
+UPDATE Accounts SET Balance = Balance - @Amount
+	WHERE Id = @SenderId;
+UPDATE Accounts SET Balance = Balance + @Amount
+	WHERE Id = @ReceiverId;
+IF @@ROWCOUNT <> 1 -- Didn’t affect exactly one row
+BEGIN
+	ROLLBACK
+	RAISERROR('Invalid operation!', 16, 2);
+	RETURN
+END
+COMMIT
+
+-------------------------------
+-- Queries for SoftUni Database
+USE SoftUni;
+-- 7. Employees with Three Projects
+CREATE PROC usp_AssignProject(@employeeId INT, @projectID INT)
+AS
+BEGIN TRANSACTION
+DECLARE @ProjectsCount INT
+SELECT @ProjectsCount = COUNT(EmployeeId) FROM EmployeesProjects WHERE EmployeeId = @employeeId;
+IF (@ProjectsCount >= 3)
+BEGIN
+	ROLLBACK
+	RAISERROR('The employee has too many projects!', 16, 1);
+	RETURN
+END
+INSERT INTO EmployeesProjects
+VALUES (@employeeId, @projectID);
+IF @@ROWCOUNT <> 1 -- Didn’t affect exactly one row
+BEGIN
+	ROLLBACK
+	RAISERROR('Invalid operation!', 16, 2);
+	RETURN
+END
+COMMIT
+
+-- 8. Delete Employees
+CREATE TABLE Deleted_Employees(
+	EmployeeId INT PRIMARY KEY, 
+	FirstName NVARCHAR(50), 
+	LastName NVARCHAR(50), 
+	MiddleName NVARCHAR(50), 
+	JobTitle NVARCHAR(100),
+	DepartmentID INT,
+	Salary DECIMAL(16,2)
+);
+
+CREATE TRIGGER tr_Employees_After_Delete ON Employees
+AFTER DELETE
+AS
+BEGIN
+INSERT INTO Deleted_Employees
+    SELECT FirstName,
+           LastName,
+           MiddleName,
+           JobTitle,
+           DepartmentID,
+           Salary
+    FROM deleted;
+END;
