fd4f9226c9a634c81a397eb8998d57558c40bb88
KardamSimeonov <masutayodagm@gmail.com>
2023-05-21 21:21:03 +0300

    update

---
+++ C#/OOP-Advanced/09.Callbacks-Exercises/ListOfPredicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace ListOfPredicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] dividers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Predicate<int> predicate = x =>
+            {
+                foreach (var divider in dividers)
+                {
+                    if (x % divider != 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            };
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (predicate(i))
+                {
+                    sb.Append(i + " ");
+                }
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
