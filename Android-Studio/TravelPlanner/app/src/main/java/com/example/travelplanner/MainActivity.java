ec47e85821cbdfe1fa0d40b7c7ff67da42420991
KardamSimeonov <masutayodagm@gmail.com>
2023-06-19 22:50:21 +0300

    update

--- Android-Studio/TravelPlanner/app/src/main/java/com/example/travelplanner/MainActivity.java
+++ Android-Studio/TravelPlanner/app/src/main/java/com/example/travelplanner/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.travelplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<PlaceItemModel> placeItemModels = new ArrayList<>();
+    String[] xIDArray = {
+            "W260653250",
+            "Q25484833",
+            "R8311615",
+            "N2907552977",
+            "W287104635",
+            "N7181741987",
+            "Q12293249",
+            "W162789700",
+            "W171437780"
+    };
+
+    String[] nameArray = {
+            "Church of St Petka of the Saddlers",
+            "Античен културно - комуникационен комплекс Сердика",
+            "Преториум",
+            "Saint Sophia",
+            "Serdika Metro Station",
+            "Street Art",
+            "Ascension of Jesus Church, Sofia",
+            "Holy Nedelya Church",
+            "Saint George Rotunda"
+    };
+
+    String[] kindsArray = {
+            "religion,churches,interesting_places,other_churches",
+            "museums,cultural,interesting_places,other_museums",
+            "historic,archaeology,interesting_places,other_archaeological_sites",
+            "historic,monuments_and_memorials,urban_environment,cultural,interesting_places,sculptures,monuments",
+            "historic,archaeology,cultural,museums,interesting_places,other_museums,other_archaeological_sites",
+            "wall_painting,cultural,urban_environment,interesting_places",
+            "religion,churches,interesting_places,other_churches",
+            "religion,cathedrals,churches,interesting_places,eastern_orthodox_churches",
+            "religion,churches,interesting_places,eastern_orthodox_churches"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.RecyclerView);
+
+        setupPlaceItemModels();
+
+        Place_RecyclerViewAdapter adapter = new Place_RecyclerViewAdapter(this, placeItemModels);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    private void setupPlaceItemModels() {
+        for (int i = 0; i < xIDArray.length; i++) {
+            String[] kinds = kindsArray[i].split(",");
+            String firstWord = kinds[0].trim();
+            placeItemModels.add(new PlaceItemModel(xIDArray[i], nameArray[i], firstWord));
+        }
+    }
+
 }
--- Android-Studio/TravelPlanner/app/src/main/java/com/example/travelplanner/Place_RecyclerViewAdapter.java
+++ Android-Studio/TravelPlanner/app/src/main/java/com/example/travelplanner/Place_RecyclerViewAdapter.java
@@ -4,13 +4,15 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class Place_RecyclerViewAdapter extends RecyclerView.Adapter<Person_RecyclerViewAdapter.MyViewHolder> {
+public class Place_RecyclerViewAdapter extends RecyclerView.Adapter<Place_RecyclerViewAdapter.MyViewHolder> {
     Context context;
     ArrayList<PlaceItemModel> placeModels;
 
@@ -21,7 +23,7 @@ public class Place_RecyclerViewAdapter extends RecyclerView.Adapter<Person_Recyc
 
     @NonNull
     @Override
-    public Person_RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public Place_RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.recycler_view_row, parent, false);
 
@@ -29,12 +31,39 @@ public class Place_RecyclerViewAdapter extends RecyclerView.Adapter<Person_Recyc
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Person_RecyclerViewAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull Place_RecyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.destinationName.setText(placeModels.get(position).getName());
+        holder.destinationCategory.setText(placeModels.get(position).getKinds());
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle card click event
+                String property = getItem(position).getProperty(); // Get the property from the clicked card
+
+                // Create an Intent and pass the property as an extra
+                Intent intent = new Intent(view.getContext(), AnotherActivity.class);
+                intent.putExtra("PROPERTY_KEY", property);
+
+                // Start the new activity
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return placeModels.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView destinationName, destinationCategory;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            destinationName = itemView.findViewById(R.id.destinationNameTextView);
+            destinationCategory = itemView.findViewById(R.id.destinationCategoryTextView);
+        }
     }
 }
