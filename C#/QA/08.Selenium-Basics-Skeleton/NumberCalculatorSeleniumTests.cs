33f18de0019a1a422899ac5fbb5b92a6342721c5
KardamSimeonov <masutayodagm@gmail.com>
2023-12-05 21:58:27 +0200

    update

--- C#/QA/08.Selenium-Basics-Skeleton/NumberCalculatorSeleniumTests.cs
+++ C#/QA/08.Selenium-Basics-Skeleton/NumberCalculatorSeleniumTests.cs
@@ -11,11 +11,21 @@ namespace SeleniumTestsProject
         IWebElement textBoxFirstNum;
         IWebElement textBoxSecondNum;
         IWebElement dropDownOperation;
-        IWebElement textBoxFirstNum;
+        IWebElement calculatiomButton;
+        IWebElement resetButton;
+        IWebElement divResult;
 
         [OneTimeSetUp]
         public void Setup()
         {
+            this.driver = new ChromeDriver();
+            this.driver.Url = "https://number-calculator.nakov.repl.co";
+            this.textBoxFirstNum = this.driver.FindElement(By.Id("number1"));
+            this.textBoxSecondNum = this.driver.FindElement(By.Id("number1"));
+            this.dropDownOperation = this.driver.FindElement(By.Id("operation"));
+            this.calculatiomButton = this.driver.FindElement(By.Id("calcButton"));
+            this.resetButton = this.driver.FindElement(By.Id("resetButton"));
+            this.divResult = this.driver.FindElement(By.Id("result"));
         }
 
         // Valid Integers
@@ -27,11 +37,21 @@ namespace SeleniumTestsProject
                                           string num2,
                                           string expected)
         {
+            resetButton.Click();
+            textBoxFirstNum.SendKeys(num1);
+            dropDownOperation.SendKeys(op);
+            textBoxSecondNum.SendKeys(num2);
+
+            calculatiomButton.Click();
+
+            string actualResult = divResult.Text;
+            Assert.AreEqual(expected, actualResult);
         }
 
         [OneTimeTearDown]
         public void TearDown()
         {
+            this.driver.Quit();
         }
     }
 }
